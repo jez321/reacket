c937c687b0c3528c7e06643e5bc3f3bf3bf97ac5
Jeremy Johns <jezjez@gmail.com>
2020-01-08 02:10:01 +0900

    add player component winner state and context call tests

--- src/components/Player/Player.test.js
+++ src/components/Player/Player.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, * as ReactAll from 'react';
 import { shallow } from 'enzyme';
 import TestUtil from '../../test/testUtil';
 import Player from './Player.component';
@@ -8,7 +8,7 @@ const defaultProps = {
   name: 'Sample player',
   seed: 2,
   score: 1,
-  winner: true,
+  winner: false,
 };
 const setup = (props = {}, state = null) => {
   const setupProps = { ...defaultProps, ...props };
@@ -20,6 +20,30 @@ const setup = (props = {}, state = null) => {
 test('renders without error', () => {
   const wrapper = setup();
   expect(wrapper).toBeTruthy();
+});
+
+test('calls setHighlightedPlayer with correct id when mouseEntered', (done) => {
+  jest.spyOn(ReactAll, 'useContext').mockImplementation(() => ({
+    highlightedPlayer: null,
+    setHighlightedPlayer: (id) => {
+      expect(id).toEqual(1);
+      done();
+    },
+  }));
+  const wrapper = setup();
+  wrapper.find('.player').simulate('mouseEnter');
+});
+
+test('is not displayed as winner when winner is false', () => {
+  const wrapper = setup();
+  expect(wrapper.find('.winner').exists()).toBeFalsy();
+});
+
+test('is displayed as winner when winner is true', () => {
+  const wrapper = setup({
+    winner: true,
+  });
+  expect(wrapper.find('.winner').exists()).toBeTruthy();
 });
 
 test('does not throw warning with expected props', () => {
