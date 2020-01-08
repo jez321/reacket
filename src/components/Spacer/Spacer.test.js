b29832701b3816f5a9e921a4f32cdae03a8a5681
Jeremy Johns <jezjez@gmail.com>
2020-01-08 09:36:09 +0900

    add spacer count test

--- src/components/Spacer/Spacer.test.js
+++ src/components/Spacer/Spacer.test.js
@@ -19,6 +19,27 @@ test('renders without error', () => {
   expect(wrapper).toBeTruthy();
 });
 
+test('renders correct number of spacers - 1', () => {
+  const wrapper = setup({
+    height: 1,
+  });
+  expect(wrapper.find('.spacer').length).toEqual(1);
+});
+
+test('renders correct number of spacers - 2', () => {
+  const wrapper = setup({
+    height: 2,
+  });
+  expect(wrapper.find('.spacer').length).toEqual(2);
+});
+
+test('renders correct number of spacers - 4', () => {
+  const wrapper = setup({
+    height: 4,
+  });
+  expect(wrapper.find('.spacer').length).toEqual(4);
+});
+
 test('does not throw warning with expected props', () => {
   TestUtil.checkProps(Spacer, defaultProps);
 });
